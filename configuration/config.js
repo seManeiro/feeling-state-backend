bb53ded180b6d61dec04247e1d2acea5bf68b447
seManeiro <sebatian.maneiro@gmail.com>
2020-07-01 15:48:26 +0200

    refactoring configuration

--- configuration/config.js
+++ configuration/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  db: "mongodb://localhost:27017/feelings",
-  cloudDb:
-    "mongodb+srv://semaneiro:x2cy.t1jCWG&DF^P6@cluster0-eoleu.mongodb.net/<dbname>?retryWrites=true&w=majority", 
+  cloudDb: process.env.MONGODB_URI, 
   secretOrKey: "secret",
 };
